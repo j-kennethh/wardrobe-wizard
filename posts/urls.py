2e361f17308b1788e0d8a71ec1e2951c76a27147
j-kennethh <johnkennethagpaoa@gmail.com>
2025-06-29 23:07:40 +0800

    Connect url to save outfit

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.posts_list, name="list"),
     path('new-post/', views.post_new, name="new-post"),
     # path('<slug:slug>', views.post_page, name="page"),
-    path('<int:pk>/', views.post_page, name="page")
+    path('<int:pk>/', views.post_page, name="page"),
+    path('save-outfit/', views.save_outfit, name="save-outfit")
 ]
