f02f91d3896eff34e68c998ae74c1c56e25f7c05
Phu-Xien <e1357004@u.nus.edu>
2025-06-29 13:06:06 +0800

    DB-Postgres

--- wardrobewizard/settings.py
+++ wardrobewizard/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'wardrobewizard.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'vRSziKedZOPRVeYgqRaSSoYSJsHslKmJ',
+        'HOST': 'interchange.proxy.rlwy.net',
+        'PORT': '12807',
     }
 }
 
