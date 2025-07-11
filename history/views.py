d28991e75f93d50609260308dacf9600552d9c39
j-kennethh <johnkennethagpaoa@gmail.com>
2025-07-12 00:09:38 +0800

    Add views to log and see outfits

--- history/views.py
+++ history/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import OutfitHistoryForm
+from .models import OutfitHistory
+from fitting_room.models import Look
 
 # Create your views here.
+@login_required
+def history(request):
+    entries = OutfitHistory.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'history/history.html', {'entries': entries})
+
+@login_required
+def log_outfit(request):
+    if request.method == 'POST':
+        form = OutfitHistoryForm(request.POST)
+        form.fields['look'].queryset = Look.objects.filter(user=request.user) # limit choices to user's looks
+        if form.is_valid():
+            outfit = form.save(commit=False)
+            outfit.user = request.user
+            outfit.save()
+            return redirect('history:history')
+    else:
+        form = OutfitHistoryForm()
+        form.fields['look'].queryset = Look.objects.filter(user=request.user)
+
+    return render(request, 'history/log_outfit.html', {'form': form})
