6eb26445e00418adb708a11af1146f9068b96cd9
Phu-Xien <e1357004@u.nus.edu>
2025-07-27 19:28:53 +0800

    Add Buttons to toggle between related pages

--- history/urls.py
+++ history/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'history'
+app_name = "history"
 
 urlpatterns = [
-    path('', views.history, name='history'),
-    path('log/', views.log_outfit, name='log_outfit')
+    path("", views.history, name="history"),
+    path("log/", views.log_outfit, name="log_outfit"),
 ]
