d4757d8bd09b0da9e28f1ea7723184e75902f37c
Phu-Xien <e1357004@u.nus.edu>
2025-07-28 01:27:41 +0800

    Unit tests for urls in closet app

---
+++ closet/tests/test_urls.py
@@ -0,0 +1,21 @@
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse, resolve
+from closet.views import clothing_items_list, clothing_item_new, clothing_item_page
+
+# Create your tests here.
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_list_url_resolves(self):
+        url = reverse("closet:list")
+        # print(resolve(url))
+        self.assertEquals(resolve(url).func, clothing_items_list)
+
+    def test_new_item_url_resolves(self):
+        url = reverse("closet:new-item")
+        self.assertEquals(resolve(url).func, clothing_item_new)
+
+    def test_page_url_resolves(self):
+        url = reverse("closet:page", kwargs={"pk": 1})
+        self.assertEquals(resolve(url).func, clothing_item_page)
